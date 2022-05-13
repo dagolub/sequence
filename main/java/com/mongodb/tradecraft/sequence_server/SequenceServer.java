b572f52bbc1aa5eb2f512293f67d0b56b9e9c0f4
Dmitry Golub <dmytro.h@smart-gamma.com>
2022-05-13 09:43:29 +0200

    update

--- main/java/com/mongodb/tradecraft/sequence_server/SequenceServer.java
+++ main/java/com/mongodb/tradecraft/sequence_server/SequenceServer.java
@@ -22,32 +22,47 @@ import com.mongodb.client.model.ReturnDocument;
 
 public class SequenceServer {
 
-	Logger logger;
+    Logger logger;
+    MongoCollection<Document> sequences;
 
-	SequenceServer() {
-		logger = LoggerFactory.getLogger(SequenceServer.class);
-        MongoCollection<Document> sequences;
-        String uri ="mongodb+srv://atlasAdmin:atlasAdmin@mission03.qud05.mongodb.net/test";
-
-        //Add code Below Here
-
-        MongoClient mongoClient = MongoClients.create(uri);
+    SequenceServer() {
+        String URI ="mongodb+srv://atlasAdmin:atlasAdmin@mission03.qud05.mongodb.net/test";
+        logger = LoggerFactory.getLogger(SequenceServer.class);
+        MongoClient mongoClient = MongoClients.create(URI);
         MongoDatabase database = mongoClient.getDatabase("mydb");
         sequences = database.getCollection("sequences");
         System.out.println("Database contains " + sequences.countDocuments() + " sequences");
-
     }
 
 	//Fetch back inforamtion about samples
-    public String sequence(Request req, Response res) {
+    public String sequence(Request req, Response res) throws InterruptedException {
         res.type("application/json");
         String name = req.params(":name");
-        //Add Code Below Here
         FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
         options.upsert(true);
         options.returnDocument(ReturnDocument.AFTER);
-        Document r = sequences.findOneAndUpdate(eq("_id",name), inc("count",1), options);
-        String json = "{" + name + ":" + r.getInteger("count") + "}";
+
+        Document r=null;
+        boolean done = false;
+        int attempts = 5;
+        while (done == false && attempts > 0) {
+            try {
+                r = sequences.findOneAndUpdate(eq("_id", name), inc("count", 1),
+                        options);
+                done = true;
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                attempts--;
+                logger.info("Attempting " + attempts + " more times");
+                Thread.sleep(2000);
+            }
+        }
+        String json;
+        if(done == true) {
+            json = "{" + name + ":" + r.getInteger("count") + "}";
+        } else { json = "{error:1}";
+        }
+        res.type("application/json");
         return json;
     }
 
